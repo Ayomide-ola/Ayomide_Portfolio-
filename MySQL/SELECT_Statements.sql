71af8ac3823b4eda079b80c398ee21e43228f767
Ayomide <147144682+Ayomide-ola@users.noreply.github.com>
2024-09-26 15:25:33 +0100

    Update SELECT_Statements.sql

--- MySQL/SELECT_Statements.sql
+++ MySQL/SELECT_Statements.sql
@@ -5,7 +5,7 @@ SELECT first_name,last_name,birth_date,
 age,
 age+10
 FROM parks_and_recreation.employee_demographics;
-# Arritmetic Order in SQL: PEMDAS [Parentesis,Equal, Multiplication, Division, Addition, Subtraction]: This is the order SQL will run multi arritmetic calculations. 
+# Arritmetic Order in SQL: PEMDAS [Parentesis,Exponent, Multiplication, Division, Addition, Subtraction]: This is the order SQL will run multi arritmetic calculations. 
 
 # Select specific column
 SELECT first_name

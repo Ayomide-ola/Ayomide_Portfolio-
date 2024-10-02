7d1ac470e4458ce07ec7e298412b5b508182f487
Ayomide <147144682+Ayomide-ola@users.noreply.github.com>
2024-10-02 16:20:23 +0100

    Exploratory

--- MySQL/Tech layoffs cleaning project/Exploratory Data Analysis.sql
+++ MySQL/Tech layoffs cleaning project/Exploratory Data Analysis.sql
@@ -69,3 +69,46 @@ ORDER BY 1 ASC
 SELECT `MONTH`, total_off,
 SUM(TOTAL_OFF) OVER(ORDER BY `MONTH`) AS rolling_total
 from Rolling_Total;
+
+
+
+SELECT company, YEAR(`date`), SUM(percentage_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+ORDER BY company ASC;
+
+SELECT company, YEAR(`date`), SUM(percentage_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+ORDER BY 3 DESC;
+
+
+WITH COMPANY_YEAR (company, years,total_liad_off) AS 
+(
+SELECT company, YEAR(`date`), SUM(total_laid_off) AS total_laid_off
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+)
+SELECT*, DENSE_RANK() OVER(PARTITION BY years ORDER BY total_liad_off DESC) AS RANKING
+FROM COMPANY_YEAR
+WHERE years IS NOT NULL
+ORDER BY RANKING ASC;
+
+WITH COMPANY_YEAR (company, years,total_liad_off) AS 
+(
+SELECT company, YEAR(`date`), SUM(total_laid_off) AS total_laid_off
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+), Company_Year_Rank AS
+
+(SELECT*, DENSE_RANK() OVER(PARTITION BY years ORDER BY total_liad_off DESC) AS RANKING
+FROM COMPANY_YEAR
+WHERE years IS NOT NULL
+)
+SELECT *
+FROM COMPANY_YEAR_RANK
+WHERE RANKING <= 5
+;
+
+
+
